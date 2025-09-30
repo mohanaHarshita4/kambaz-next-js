67270809e9e5e35507596852b5eb8f286f2ab203
mohanaHarshita4 <159101207+mohanaHarshita4@users.noreply.github.com>
2025-09-29 22:08:05 -0400

    added default values

--- app/(Kambaz)/Account/SignIn/page.tsx
+++ app/(Kambaz)/Account/SignIn/page.tsx
@@ -3,8 +3,8 @@ export default function SignIn() {
  return (
    <div id="wd-signin-screen">
       <h3>Sign in</h3>
-      <input className="wd-username" placeholder="username" /> <br />
-      <input className="wd-password" placeholder="password" type="password" /> <br />
+      <input className="wd-username" placeholder="username" value="john" /> <br />
+      <input className="wd-password" placeholder="password" type="password" value="1234"/> <br />
       <Link id="wd-signin-btn" href="/DashBoard"> Sign in </Link> <br />
       <Link id="wd-signup-link" href="/Account/SignUp"> Sign up </Link>
     </div>
--- app/(Kambaz)/Account/SignUp/page.tsx
+++ app/(Kambaz)/Account/SignUp/page.tsx
@@ -3,10 +3,10 @@ export default function SignUp() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" className="wd-username" /><br/>
-      <input placeholder="password" type="password" className="wd-password" /><br/>
+      <input placeholder="username" className="wd-username" value="john" /><br/>
+      <input placeholder="password" type="password" className="wd-password" value="1234" /><br/>
       <input placeholder="verify password"
-             type="password" className="wd-password-verify" /><br/>
+             type="password" className="wd-password-verify" value="1234" /><br/>
       <Link  href="Profile" > Sign up </Link><br />
       <Link  href="SignIn" > Sign in </Link>
     </div>
